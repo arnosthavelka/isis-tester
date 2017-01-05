c7c45a8bd7db0e2faaa30748d685dc147d695f4b
Arnošt Havelka <arnost.havelka@gmail.com>
2017-01-05 10:37:08 +0100

    + collection mapping added

--- src/main/java/com/github/aha/isis/tester/dto/Settings.java
+++ src/main/java/com/github/aha/isis/tester/dto/Settings.java
@@ -1,12 +1,17 @@
 package com.github.aha.isis.tester.dto;
 
 import java.io.Serializable;
+import java.util.Collection;
 
 public class Settings implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
 	private boolean fooEnabled;
+	
+	private Param initParam;
+	
+	private Collection<Param> params;
 
     public boolean isFooEnabled() {
         return fooEnabled;
@@ -15,5 +20,21 @@ public class Settings implements Serializable {
     public void setFooEnabled(boolean fooEnabled) {
         this.fooEnabled = fooEnabled;
     }
+
+	public Collection<Param> getParams() {
+		return params;
+	}
+
+	public void setParams(Collection<Param> params) {
+		this.params = params;
+	}
+
+	public Param getInitParam() {
+		return initParam;
+	}
+
+	public void setInitParam(Param initParam) {
+		this.initParam = initParam;
+	}
 
 }
--- src/test/java/com/github/aha/isis/tester/IsisTesterApplicationTests.java
+++ src/test/java/com/github/aha/isis/tester/IsisTesterApplicationTests.java
@@ -9,7 +9,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.CollectionUtils;
 
+import com.github.aha.isis.tester.dto.Param;
 import com.github.aha.isis.tester.dto.Settings;
 import com.github.aha.isis.tester.service.XmlMarshaller;
 
@@ -31,5 +33,15 @@ public class IsisTesterApplicationTests {
 	public void loadXML() throws IOException {
         Settings dto = (Settings) this.xmlMarshaller.loadXML(FILE_NAME);
         LOG.debug("fooEnabled={}" , dto.isFooEnabled());
+        Param initParam = dto.getInitParam();
+		if (initParam != null) {
+        	LOG.debug("initParam [name={}, name={}]" , initParam.getName(), initParam.getValue());
+        }
+        if (!CollectionUtils.isEmpty(dto.getParams())) {
+        	LOG.debug("no. of params={}" , dto.getParams().size());
+        	for (Param param : dto.getParams()) {
+        		LOG.debug("param [name={}, name={}]" , param.getName(), param.getValue());
+			}
+        }
 	}
 }
