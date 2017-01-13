f5d9b52ef5b00045d19f9e02ca4a53d776ffd7cd
Arnošt Havelka <arnost.havelka@gmail.com>
2017-01-13 15:07:22 +0100

    + dynamic loading of XML based on configured path

--- src/main/java/com/github/aha/isis/tester/service/TestManager.java
+++ src/main/java/com/github/aha/isis/tester/service/TestManager.java
@@ -1,10 +1,16 @@
 package com.github.aha.isis.tester.service;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
 import org.springframework.stereotype.Service;
 
 import com.github.aha.isis.tester.dto.IsisTestDTO;
@@ -12,22 +18,38 @@ import com.github.aha.isis.tester.dto.IsisTestDTO;
 @Service
 public class TestManager {
 	
-	@Autowired
+    @Value("${data.xml.path}")
+    private String xmlPath;
+    
+    @Autowired
+    ResourceLoader resourceloader;
+    
+    @Autowired
 	private XmlMarshaller xmlMarshaller;	
 	
 //	@Cacheable("menu")
 	public Collection<String> getTests() {
 		Collection<String> items = new ArrayList<>();
-		items.add("3PA523");
-		items.add("3PA541");
-		items.add("3PA542");
-		items.add("3PA546");
+		
+		Resource[] resources;
+		try {
+			ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver(this.getClass().getClassLoader());
+			resources = resolver.getResources(xmlPath + "/*.xml");
+			for (Resource resource: resources){
+				String filename = resource.getFilename();
+				// get just name (not file extension)
+				items.add(filename.substring(0, filename.indexOf(".")));
+			}		
+		} catch (IOException e) {
+			throw new RuntimeException("Retrieving XML files error", e);
+		}
 		return items;
 	}
 
 	@Cacheable("tests")
 	public IsisTestDTO loadData(String testID) {
-		IsisTestDTO dto = (IsisTestDTO) this.xmlMarshaller.loadXML(String.format("%s_test.xml", testID));
+		// add file extension to match XML file4
+		IsisTestDTO dto = (IsisTestDTO) this.xmlMarshaller.loadXML(String.format("%s.xml", testID));
 		return dto;
 	}
 
