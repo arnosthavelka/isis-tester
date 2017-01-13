5a86c0dc128763d281ef96981c0d4b658f7ffe63
Arnošt Havelka <arnost.havelka@gmail.com>
2017-01-13 14:19:55 +0100

    + menu rendering according to data from test manager service

--- src/main/java/com/github/aha/isis/tester/controller/HomeController.java
+++ src/main/java/com/github/aha/isis/tester/controller/HomeController.java
@@ -1,10 +1,16 @@
 package com.github.aha.isis.tester.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.github.aha.isis.tester.service.TestManager;
 
 @Controller
 @RequestMapping("/")
@@ -13,10 +19,17 @@ public class HomeController {
 	@Value("#{versionService.appVersion}")
 	private String version;
 	
-
+	@Autowired
+	private TestManager manager;
+	
 	@GetMapping
 	public ModelAndView list() {
-		return new ModelAndView("views/home", "appVersion", version);
+		
+		Map<String, Object> params = new HashMap<>();
+		params.put("menuItems", manager.getTests());
+		params.put("appVersion", version);
+		
+		return new ModelAndView("views/home", params);
 	}
 	
 }
--- src/main/java/com/github/aha/isis/tester/service/TestManager.java
+++ src/main/java/com/github/aha/isis/tester/service/TestManager.java
@@ -1,5 +1,8 @@
 package com.github.aha.isis.tester.service;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
@@ -12,6 +15,16 @@ public class TestManager {
 	@Autowired
 	private XmlMarshaller xmlMarshaller;	
 	
+//	@Cacheable("menu")
+	public Collection<String> getTests() {
+		Collection<String> items = new ArrayList<>();
+		items.add("3PA523");
+		items.add("3PA541");
+		items.add("3PA542");
+		items.add("3PA546");
+		return items;
+	}
+
 	@Cacheable("tests")
 	public IsisTestDTO loadData(String testID) {
 		IsisTestDTO dto = (IsisTestDTO) this.xmlMarshaller.loadXML(String.format("%s_test.xml", testID));
