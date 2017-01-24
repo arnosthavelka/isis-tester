63726cb25aa11fba56e60b3e6615eb1f2e2c2a0f
Arnošt Havelka <arnost.havelka@gmail.com>
2017-01-24 10:23:50 +0100

    + "tuning" and added hints in i18n configuration

--- src/main/java/com/github/aha/isis/tester/IsisTesterApplication.java
+++ src/main/java/com/github/aha/isis/tester/IsisTesterApplication.java
@@ -14,8 +14,8 @@ import org.springframework.oxm.castor.CastorMarshaller;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 @EnableCaching
@@ -48,15 +48,22 @@ public class IsisTesterApplication extends WebMvcConfigurerAdapter {
     
     @Bean
     public LocaleResolver localeResolver() {
-        SessionLocaleResolver slr = new SessionLocaleResolver();
-        slr.setDefaultLocale(Locale.forLanguageTag("cs"));
-        return slr;
+    	// we can use SessionLocaleResolver as well
+        CookieLocaleResolver resolver = new CookieLocaleResolver();
+        resolver.setDefaultLocale(Locale.forLanguageTag("cs"));
+        return resolver;
     }
+    
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+		// we can use the default name ("locale") as well
+		localeChangeInterceptor.setParamName("lang");
+		return localeChangeInterceptor;
+	}
     
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-		LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
-		interceptor.setParamName("lang");
-		registry.addInterceptor(interceptor);
+		registry.addInterceptor(localeChangeInterceptor());
     }
 }
